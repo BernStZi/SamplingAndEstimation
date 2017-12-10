5d4b789f7745edda4145300209175e31def2c661
zins <stefan.zins@gesis.org>
2017-12-10 20:03:02 +0100

    lecture slides edited

--- SamEffectExam.R
+++ SamEffectExam.R
@@ -5,8 +5,9 @@ library(sampling)
 
 select.apiTwoStage <- 
   function(n_I, n_c, pop){
-    apipop_mss <- pop
-    h_c  <- table(apipop_mss$cnum)/nrow(apipop_mss)*n_I
+    data(api)
+    dat <- apipop
+    h_c  <- table(dat$cnum)/nrow(dat)*n_I
     
     
     p_c  <- h_c%%1
@@ -17,12 +18,12 @@ select.apiTwoStage <-
     
     s.snum <- unlist(
       mapply(function(x,y) sample(as.character(x), y), 
-             tapply(apipop_mss$snum,apipop_mss$cnum,identity)[names(n_i)],
+             tapply(dat$snum,dat$cnum,identity)[names(n_i)],
              n_i
       )
     )
     
-    h_ci  <- h_c[apipop_mss$cnum]
+    h_ci  <- h_c[as.character(dat$cnum)]
     n_i.m <- rep(n_c,length(h_ci))
     
     n_i.m[h_ci>1] <- h_ci[h_ci>1]*n_c
@@ -30,11 +31,11 @@ select.apiTwoStage <-
     p_ci         <- h_ci
     p_ci[h_ci>1] <- 1
     
-    apipop_mss$fpc2 <- n_i.m[apipop_mss$cnum]/table(apipop_mss$cnum)[as.character(apipop_mss$cnum)]
-    apipop_mss$fpc1 <- p_ci[as.character(apipop_mss$cnum)]
+    dat$fpc2 <- n_i.m[dat$cnum]/table(dat$cnum)[as.character(dat$cnum)]
+    dat$fpc1 <- p_ci[as.character(dat$cnum)]
     
     
-    apiclus3 <- apipop_mss[apipop_mss$snum%in%as.numeric(s.snum),]
+    apiclus3 <- dat[dat$snum%in%as.numeric(s.snum),]
     
     svydesign(id=~cnum+snum, fpc = ~fpc1 + fpc2, data=apiclus3, pps = "brewer")
     
@@ -46,17 +47,49 @@ select.apiTwoStage <-
 
 select.apiClu <- 
   function(n_I){
-    
-    s.snum <- sample(as.character(apipop_mss$dnum), n_I)
+    data(api)
+    dat <- apipop
+    s.dnum <- sample(as.character(dat$dnum), n_I)
         
-    apipop_mss$fpc1 <- length(unique(apipop_mss$dnum))
+    dat$fpc1 <- length(unique(dat$dnum))
     
-    apiclus         <- apipop_mss[apipop_mss$dnum %in% as.numeric(s.snum), ]
+    apiclus         <- dat[dat$dnum %in% as.numeric(s.dnum), ]
     
     svydesign(id=~dnum, fpc = ~fpc1, data=apiclus)
     
   }
 
+rand.round <- function(x){
+  
+  int <- x%/%1
+  round.int <- sampling::UPbrewer(x%%1)
+  rannum <- runif(length(x))
+  int+round.int
+  
+}
+
+select.apiStra <- 
+  function(n){
+    #prob allocation
+    data(api)
+    dat <- apipop
+    nh_ <- rand.round(table(dat$cnum)/nrow(dat)*n)
+    nh  <- nh_
+    nh[nh_<2] <- 2
+    
+    s.snum <- unlist(
+      mapply(function(x,y) sample(as.character(x), y), 
+             tapply(dat$snum,dat$cnum,identity)[names(nh)],
+             nh
+      )
+    )
+    
+    dat$fpc1 <- table(dat$cnum)[as.character(dat$cnum)]
+    apistra  <- dat[dat$snum %in% as.numeric(s.snum), ]
+    
+    svydesign(id=~snum, fpc = ~fpc1, strata = ~cnum, data=apistra)
+    
+  }
 
 
 #design object for rotational panels
@@ -71,8 +104,8 @@ set.seed(1341)
 tv <- mean(apipop$api00-apipop$api99)
 
 mu_Ho <- tv
-apipop_mss <- apipop
-#apipop_mss$api00 <- apipop_mss$api99+rnorm(nrow(apipop_mss),0,sd(apipop_mss$api99)^{1/2})
+
+#dat$api00 <- dat$api99+rnorm(nrow(dat),0,sd(dat$api99)^{1/2})
 
 
 OUT <- 
@@ -97,8 +130,8 @@ svyH0    <- svyConf.int[1] < mu_Ho & svyConf.int[2] > mu_Ho
 
 
 var.delta.sim <- 
-  var(dclus_1$variables$api00)/nrow(dclus_1$variables)*(1-nrow(dclus_1$variables)/6194) + 
-  var(dclus_2$variables$api00)/nrow(dclus_2$variables)*(1-nrow(dclus_2$variables)/6194)
+  var(dclus_1$variables$api00)/nrow(dclus_1$variables)+ 
+  var(dclus_2$variables$api00)/nrow(dclus_2$variables)
 
 
 svyConf.sim <- c(delta + qnorm(1-0.975)*sqrt(var.delta.sim ),
@@ -109,10 +142,9 @@ svyConf.sim <- c(delta + qnorm(1-0.975)*sqrt(var.delta.sim ),
 #                   y = dclus_2$variables$api99, 
 #                   var.equal = FALSE, 
 #                   mu = mu_Ho)
+#simH0    <- simConf$p.value < 0.05
 
 simH0    <- svyConf.sim[1] < mu_Ho & svyConf.sim[2] > mu_Ho
-
-#simH0    <- simConf$p.value < 0.05
 
 c(svyH0, simH0, delta)
 
